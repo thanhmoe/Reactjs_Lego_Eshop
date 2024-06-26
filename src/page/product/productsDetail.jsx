b2b4c257252044a0aac2df6d2b84898bbe48295f
Thanh Nguyễn <thanhnct24@gmail.com>
2024-06-26 23:36:35 +0700

    feat: view detail product with mock api, add mock api file

--- src/page/product/productsDetail.jsx
+++ src/page/product/productsDetail.jsx
@@ -1,94 +1,105 @@
 import React, { useEffect, useState } from "react";
-import './productsDetail.css';
+import './productsDetail.css'
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { CommentOutlined } from '@ant-design/icons';
+// import { selectProducts, fetchProduct, selectLoadingState } from "../../redux/slice/products/productsSlice.js";
+import { fetchMockProduct,selectProducts,selectErrorState } from "../../redux/slice/products/productMockSlice";
 import LoadingModal from "../../modal/loadingModal.jsx";
 import CartIcon from '/src/assets/icons/cart.svg?react';
 import { Image } from "antd";
-//mock api
-import { selectProducts, selectLoadingState, fetchMockProduct } from "../../redux/slice/products/productMockSlice.js";
 
-//for actual API
+export default function productsDetail() {
+    const [open, setOpen] = useState(false);
+    const products = useSelector(selectProducts);
+    const dispatch = useDispatch();
+    //get product's ID
+    const { productId } = useParams();
+    const thisProduct = products.find(p => p.id === productId);
+
+    //for actual API
 // import { selectProducts, selectLoadingState } from "../../redux/slice/products/productsSlice.js";
 // const IMAGE_BASE_URL = import.meta.env.VITE_BASE_URL;
 
-export default function ProductsDetail() {
-    const products = useSelector(selectProducts);
-    const dispatch = useDispatch();
-    const { productId } = useParams();
-    const [thisProduct, setThisProduct] = useState(null);
-    const [loading, setLoading] = useState(true); // Local loading state
 
-
+    //check product data if visible or not
     useEffect(() => {
         if (products.length === 0) {
             dispatch(fetchMockProduct())
         };
     }, []);
-    return (
-        <>
-            {/* {loading || !thisProduct ? (
-                <LoadingModal />
-            ) : (
-                <div className="product-detail-container">
-                    <div className="detail-container">
-                        <div className="productsDetail">
-                            <div className="leftInfo">
-                                <Image src={`${IMAGE_BASE_URL}${thisProduct.image_path}`} alt={thisProduct.name} />
-                            </div>
-                            <div className="detailInfo">
-                                <h3 className="productName">{thisProduct.name}</h3>
-                                <div className="product-detail-button">
-                                    <button className="btn-buy">
-                                        <CartIcon /> Add to cart
-                                    </button>
-                                </div>
+
+    //comment state
+    const [value, setValue] = useState('')
+    const [listComment, setListComment] = useState([
+        {
+            id: 1,
+            text: 'first comment here'
+        },
+        {
+            id: 2,
+            text: ' im a second'
+        },
+        {
+            id: 3,
+            text: ' im a third guys here'
+        },
+
+    ])
+    const onSubmit = () => {
+        if (value) {
+            let nextComment = {
+                id: crypto.randomUUID(),
+                text: value,
+            }
+            const nextComments = [...listComment, nextComment]
+            setListComment(nextComments)
+            setValue('')
+        }
+    }
+
+    //handle open modal
+    const handleOpen = () => { setOpen(true) }
+    const handleClose = () => { setOpen(false) }
+
+    return <>
+        {products.length === 0
+            ? <LoadingModal />
+            :
+            <div className="product-detail-container">
+                <div className="detail-container">
+                    <div className="productsDetail">
+                        <div className="leftInfo">
+                            <Image src={thisProduct.image} />
+                        </div>
+                        <div className="detailInfo">
+                            <h3 className="productName">{thisProduct.name_product}</h3>
+                            <p>Description:{thisProduct.introduce}</p>
+                            <div className="product-detail-button">
+                                <button className="btn-buy"><CartIcon /> Add to cart</button>
                             </div>
                         </div>
                     </div>
                 </div>
-            )} */}
-            {products.length === 0
-                ? <LoadingModal />
-                :
-                <div className="product-detail-container">
-                    <div className="detail-container">
-                        <div className="productsDetail">
-                            <div className="leftInfo">
-                                <Image src={thisProduct.image} />
-                            </div>
-                            <div className="comment-component">
-                                <input className='inputCmt' placeholder="write comment here"></input>
-                                <button className="btn-cmt"> Submit</button>
-                                <div className="detailInfo">
-                                    <h3 className="productName">{thisProduct.name_product}</h3>
-                                    <p>Description:{thisProduct.introduce}</p>
-                                    <div className="product-detail-button">
-                                        <button className="btn-buy"><CartIcon /> Add to cart</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="comment">
-                            <h3>
-                                Comment
-                            </h3>
-                            <div  >
-                                {listComment.map(comment => (
-                                    <ul className="commented" key={comment.id}>
-                                        <li><CommentOutlined />
-                                            <p>{comment.text}</p>
-                                        </li>
-                                    </ul>
-                                ))}
-                            </div>
-                            <div className="comment-component">
-                                <input className='inputCmt' placeholder="write comment here"></input>
-                                <button className="btn-cmt"> Submit</button>
-                            </div>
-                        </div>
+                <div className="comment">
+                    <h3>
+                        Comment
+                    </h3>
+                    <div  >
+                        {listComment.map(comment => (
+                            <ul className="commented" key={comment.id}>
+                                <li><CommentOutlined />
+                                    <p>{comment.text}</p>
+                                </li>
+                            </ul>
+                        ))}
                     </div>
-            }
-                </>
-    );
+                    <div className="comment-component">
+                        <input className='inputCmt' placeholder="write comment here"></input>
+                        <button className="btn-cmt"> Submit</button>
+                    </div>
+                </div>
+            </div>
+        }
+    </>
 }
