31c1da9a7bea06f31438996d9a580e0427665b49
thanhmoe <thanhmoe24@gmail.com>
2024-07-08 17:26:21 +0700

    style: change contact page content, add carousel in main page

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,38 @@ import { useNavigate } from 'react-router-dom'
 import './App.css'
 import { cookies, notify } from './main'
 import { useTranslation } from "react-i18next"
-import homeimg from '/public/assets/home.png'
-
+import { Carousel } from 'antd'
 
 function App() {
   const navigate = useNavigate();
   const { t } = useTranslation(['common']);
-  const token = localStorage.getItem('auth_token')
-
+  const contentStyle = {
+    margin: 0,
+    height: '500px',
+    color: '#fff',
+    // lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
 
   return (
     <>
-      <section>
-        <h2 className="slide-in-left">{t('Welcome')}</h2>
-        <p className="slide-in-left short-paragraph">Hi, I'm a web developer with a passion for creating interactive and visually appealing websites. This is a sample homepage to demonstrate some basic web development techniques.</p>
-      </section>
-      <section>
-        <img src={homeimg} alt="A beautiful scenery" className="slide-in-right animated-image"></img>
-      </section>
+      <div className='carosel container'>
+        <Carousel autoplay arrows infinite={true}>
+          <div>
+            <h3 style={contentStyle}>1</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>2</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>3</h3>
+          </div>
+          <div>
+            <h3 style={contentStyle}>4</h3>
+          </div>
+        </Carousel>
+      </div>
     </>
   )
 }
