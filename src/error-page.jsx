17c5b88f62559eec593da0ae27c2f8bd29148cf0
thanhmoe <thanhmoe24@gmail.com>
2024-07-17 16:55:49 +0700

    (style) restyle 404 page

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,23 @@
 import { useRouteError } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import { Result, Button } from "antd";
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  const navigate = useNavigate()
 
   return (
-    <div className="errorpage" id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    // <div className="errorpage" id="error-page">
+    //   <h1>Oops!</h1>
+    //   <p>Sorry, an unexpected error has occurred.</p>
+    //   <p>
+    //     <i>{error.statusText || error.message}</i>
+    //   </p>
+    // </div>
+    <Result
+      style={{ minHeight: '100vh' }}
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button onClick={() => navigate('/')} type="primary">Back Home</Button>}
+    />
   );
 }
