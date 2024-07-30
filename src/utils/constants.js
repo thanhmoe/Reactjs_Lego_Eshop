d140a94a3038ba460c8fb11acaff2e8df4be5c23
ThanhNC <thanhmoe24@gmail.com>
2024-07-30 11:46:36 +0700

    (orders) add style for cancelled class

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -99,7 +99,7 @@ export const OKBUTTON = {
 }
 
 export const STATUSCOLORS = {
-    cancel: 'badge-canceled',
+    cancelled: 'badge-cancelled',
     delivered: 'badge-delivered',
     pending: 'badge-pending',
     shipping: 'badge-shipping'
