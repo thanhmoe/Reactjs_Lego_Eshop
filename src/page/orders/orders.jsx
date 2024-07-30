dc5ada349d9f4b1958c76d0056f6983e636dbb70
ThanhNC <thanh.nc@mor.com.vn>
2024-07-30 11:16:26 +0700

    (orders) change value key from 'cancel' to 'cancelled

--- src/page/orders/orders.jsx
+++ src/page/orders/orders.jsx
@@ -73,7 +73,7 @@ const Orders = () => {
             case '5':
                 return 'delivered';
             case '6':
-                return 'cancel';
+                return 'cancelled';
             default:
                 return null;
         }
