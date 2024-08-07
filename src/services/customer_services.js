7cf9fcb95d63d2b39aa432f9434ec388d649c9f6
Thanh Nguyễn <thanhnct24@gmail.com>
2024-08-07 23:04:54 +0700

    (login-fb) add login with facebook function

--- src/services/customer_services.js
+++ src/services/customer_services.js
@@ -1,5 +1,12 @@
+import axios from "axios";
 import { axios_instance } from "./axios_config";
 const API_PATH = '/customers'
+
+
+const baseURL = import.meta.env.VITE_BASE_URL;
+export const instance = axios.create({
+    baseURL: baseURL
+});
 
 export const fetchCustomers = async (user) => {
     try {
@@ -34,5 +41,29 @@ export const getCustomerAddress = async () => {
         return response.data;
     } catch (error) {
         return error.response.data;
+    }
+};
+
+
+export const facebookLogin = async (accessToken) => {
+    try {
+        const response = await instance.post(`${API_PATH}/facebook-login`, { accessToken });
+
+        if (response.status === 200) {
+            return {
+                success: true,
+                authToken: response.data.auth_token,
+            };
+        } else {
+            return {
+                success: false,
+                message: response.data.message || 'Facebook login failed',
+            };
+        }
+    } catch (error) {
+        return {
+            success: false,
+            message: error.response?.data?.message || 'An error occurred during Facebook login',
+        };
     }
 };
